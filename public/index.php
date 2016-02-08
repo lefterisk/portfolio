595c0dd9dd9f70fd0915539ea67a2c5985f430f5
Lefteris Kokkonas <lefterisk@Lefteriss-MBP.home>
2016-02-08 12:43:02 +0000

    Amends

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
+<?php
+    $name        = "Lefteris Kokkonas";
+    $title       = "Lefteris Kokkonas, web professional";
+    $description = "Lefteris Kokkonas is a web developer and entrepreneur. Specialises in building complex data management and visualisation platforms.";
+?>
 <!DOCTYPE html>
 <html ng-app="Portfolio">
 <head profile="http://www.w3.org/2005/10/profile">
@@ -8,6 +13,21 @@
     <link rel="stylesheet" href="./css/vendor.min.css">
     <link rel="stylesheet" href="./css/app.css">
     <link href='https://fonts.googleapis.com/css?family=Megrim|Lato:400,300' rel='stylesheet' type='text/css'>
+
+    <title><?php echo $title; ?></title>
+    <meta name="description" content="<?php echo $description; ?>">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@LKokkonas">
+    <meta name="twitter:creator" content="<?php echo $name; ?>">
+    <meta name="twitter:title" content="<?php echo $title; ?>">
+    <meta name="twitter:description" content="<?php echo $description; ?>">
+    <meta name="twitter:image:src" content="http://www.lefterisk.com/img/brand.png">
+    <meta property="og:url" content="http://www.lefterisk.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="http://www.lefterisk.com/img/brand.png">
+    <meta property="og:description" content="<?php echo $description; ?>">
+    <meta property="og:site_name" content="<?php echo $name; ?>">
+    <meta name="og:title" content="<?php echo $title; ?>" />
 </head>
 <body>
 <div class="outerFrame">
@@ -28,15 +48,15 @@
     "sameAs" : [
         "http://www.facebook.com/lefteris.kokkonas",
         "http://www.linkedin.com/in/lefteris-kokkonas-8b853845",
-        "http://www.github.com/lefterisk",
-    ]
+        "http://www.github.com/lefterisk"
+    ],
     "mainEntityOfPage": {
         "@type": "WebPage",
-        "@id": "http://www.lefterisk.com/about",
+        "@id": "http://www.lefterisk.com/about"
     },
     "birthDate": "1985-03-27",
     "jobTitle": "Senior web-developer, UX Consultant",
-    "description" : "Lefteris Kokkonas is a web developer and entrepreneur. Specialises in building complex data management and visualisation platforms."
+    "description" : "Lefteris Kokkonas is a web developer and entrepreneur. Specialises in building complex data management and visualisation platforms.",
     "image" : "http://www.lefterisk.com/img/brand.png"
 }
 </script>
